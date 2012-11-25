e2f089c8a0ff6c3c37c928b20666e81401b734a8
ujihisa <ujihisa@gmail.com>
2012-11-24 22:59:11 -0800

    tweet-mc68

--- src/mc68/core.clj
+++ src/mc68/core.clj
@@ -1,6 +1,12 @@
-(ns mc68.core)
+(ns mc68.core
+  (:require [twitter.oauth]
+            [twitter.api.restful])
+  (:import (twitter.callbacks.protocols SyncSingleCallback)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn tweet-mc68 [msg]
+  (let [creds (twitter.oauth/make-oauth-creds
+               "h20bKkS1RdDYtZeinOyHw"
+               "nS5nMR0ZbMAQAJA8Tcd6GL76qpWPvC2vPSuJN5MY4"
+               "969500324-EKgYL4bcB2Tv1ovaux5aaaBTGqj36ekrJFq7aLnt"
+               "pPASxxTT4xr0JzwaOobY0XcTzl1BME9iyU7CK3VRnw")]
+    (twitter.api.restful/update-status :oauth-creds creds :params {:status msg})))
