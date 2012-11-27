46c10385b14bcd700610682f331219d57aaba66c
ujihisa <ujihisa@gmail.com>
2012-11-26 21:21:03 -0800

    omgomg

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [twitter-api "0.6.13"]
-                 [swank-clojure/swank-clojure "1.4.2"]]
+                 [swank-clojure/swank-clojure "1.4.2"]
+                 [org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
   :dev-dependencies [[org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
