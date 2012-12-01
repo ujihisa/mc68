ee8f3e264cc8b7b1fb32ca4eb4e9d7ad087f67d5
ujihisa <ujihisa@gmail.com>
2012-11-30 18:13:14 -0800

    many

--- src/mc68/core.clj
+++ src/mc68/core.clj
@@ -2,7 +2,8 @@
   (:require [twitter.oauth]
             [twitter.api.restful]
             [swank.swank])
-  (:import (twitter.callbacks.protocols SyncSingleCallback)))
+  (:import (twitter.callbacks.protocols SyncSingleCallback)
+           [org.bukkit Bukkit]))
 
 (defn tweet-mc68 [msg]
   (let [creds (twitter.oauth/make-oauth-creds
@@ -12,6 +13,33 @@
                "pPASxxTT4xr0JzwaOobY0XcTzl1BME9iyU7CK3VRnw")]
     (twitter.api.restful/update-status :oauth-creds creds :params {:status msg})))
 
+(defn ujm []
+  (Bukkit/getPlayer "ujm"))
+(defn mozukusoba []
+  (Bukkit/getPlayer "mozukusoba"))
+(defn sixeight []
+  (Bukkit/getPlayer "Sixeight6680"))
+(defn ast []
+  (Bukkit/getPlayer "ast924"))
+(defn raa []
+  (Bukkit/getPlayer "raa0121"))
+
+(defn projectile-hit-event [evt]
+  (defn hp2damage [n]
+    (inc (Math/pow (/ (- 20 n) 4) 2)))
+  (let [proj (.getEntity evt)
+        shooter (.getShooter proj)]
+    (when (and
+            (instance? org.bukkit.entity.Arrow proj)
+            (instance? org.bukkit.entity.Player shooter))
+      (doseq [t (.getNearbyEntities proj 5 3 5)
+              :when (instance? org.bukkit.entity.Monster t)]
+        (.damage t
+                 (if (= (ujm) shooter)
+                   19
+                   (hp2damage (.getHealth shooter)))
+                 shooter)))))
+
 (defn player-login-event [evt]
   (let [player (.getPlayer evt)]
     (future
@@ -19,12 +47,16 @@
 
 (defn async-player-chat-event [evt]
   (let [pname (.getName (.getPlayer evt))]
-    (when-let [msg (second (first (re-seq #"^t:\s(.*)" (.getMessage evt))))]
+    (future
+      (tweet-mc68 (format "<%s>: %s" pname (.getMessage evt))))
+    #_(when-let [msg (second (first (re-seq #"^t:\s(.*)" (.getMessage evt))))]
       (future
         (tweet-mc68 (format "<%s>: %s" pname msg))))))
 
 (defonce swank* nil)
+(defonce plugin* nil)
 (defn on-enable [plugin]
+  (def plugin* plugin)
   (when-not swank*
     (def swank* (swank.swank/start-repl 4006))))
 
