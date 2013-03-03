ec1568bd674ecf73301112741e53eb50e0659c8e
raa0121 <raa0121@gmail.com>
2013-03-04 05:32:57 +0900

    del keys in src

--- src/mc68/core.clj
+++ src/mc68/core.clj
@@ -42,6 +42,8 @@
 (defonce plugin* nil)
 (def creative (atom #{}))
 
+(def oauth (clojure.string/split #"\n" (strip "oauth")))
+
 (defmacro later [& exps]
   `(.scheduleSyncDelayedTask
      (Bukkit/getScheduler)
@@ -50,11 +52,7 @@
      0))
 
 (defn tweet-mc68 [msg]
-  (let [creds (twitter.oauth/make-oauth-creds
-               "h20bKkS1RdDYtZeinOyHw"
-               "nS5nMR0ZbMAQAJA8Tcd6GL76qpWPvC2vPSuJN5MY4"
-               "969500324-EKgYL4bcB2Tv1ovaux5aaaBTGqj36ekrJFq7aLnt"
-               "pPASxxTT4xr0JzwaOobY0XcTzl1BME9iyU7CK3VRnw")]
+  (let [creds (twitter.oauth/make-oauth-creds oauth)]
     (twitter.api.restful/update-status :oauth-creds creds :params {:status msg})))
 
 (defn ujm []
