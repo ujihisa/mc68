55fd92c92ff33bd5eab99ae885adfeeabbec5640
ujihisa <ujihisa@gmail.com>
2013-07-15 19:32:13 -0700

    don't depend on dynmap

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL-3 or later"}
   :dependencies [[cloft "1.0.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.1"]
                  [twitter-api "0.6.13"]
                  #_[org.apache.sanselan/sanselan "0.97-incubator"]
-                 [org.dynmap/DynmapCoreAPI "1.5"]
+                 #_[org.dynmap/DynmapCoreAPI "1.5"]
                  #_[org.dynmap/dynmap-api "1.5"]]
   :dev-dependencies [[org.bukkit/bukkit "1.5-R0.1-SNAPSHOT"]]
   :java-source-paths ["javasrc"])
